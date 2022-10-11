71acb6d214b70a77c6dddd7796e2dfbbe8f30194
Subin Mohan S <subinmohan77@gmail.com>
2022-10-11 10:05:55 +0530

    automated copywright year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+from datetime import date
 
 npoint_url = 'https://api.npoint.io/a695600da11dfa61ec4f'
 posts = requests.get(npoint_url).json()
@@ -10,7 +11,7 @@ app = Flask(__name__)
 @app.route('/index.html')
 def get_all_posts():
     """Landing Page."""
-    return render_template('index.html', all_posts=posts)
+    return render_template('index.html', all_posts=posts, year=year, month=month)
 
 
 @app.route('/post/<int:index>')
@@ -19,19 +20,19 @@ def show_post(index):
     for blog_post in posts:
         if blog_post['id'] == index:
             requested_post = blog_post
-    return render_template('post.html', post=requested_post)
+    return render_template('post.html', post=requested_post, year=year)
 
 
 @app.route('/about')
 def about():
     """About Page."""
-    return render_template('about.html')
+    return render_template('about.html', year=year)
 
 
 @app.route('/contact')
 def contact():
     """Contact Page."""
-    return render_template('contact.html')
+    return render_template('contact.html', year=year)
 
 
 @app.route('/post')
@@ -41,4 +42,7 @@ def post():
 
 
 if __name__ == '__main__':
+    today = date.today()
+    year = today.year
+    month = today.month
     app.run(debug=True)

1fdafc77d5285e16b1849c342aea8ae3ec4f998a
Subin Mohan S <subinmohan77@gmail.com>
2022-10-12 16:38:52 +0530

    Created form funcitonality

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 from datetime import date
 
 npoint_url = 'https://api.npoint.io/a695600da11dfa61ec4f'
+OWN_EMAIL = 'YOUR EMAIL'
+OWN_PASSWORD = 'YOUR PASSWORD'
 posts = requests.get(npoint_url).json()
 app = Flask(__name__)
 
@@ -29,16 +32,30 @@ def about():
     return render_template('about.html', year=year)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     """Contact Page."""
-    return render_template('contact.html', year=year)
+    if request.method == 'POST':
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template('contact.html', year=year, msg_sent=True)
+    return render_template('contact.html', year=year, msg_sent=False)
 
 
 @app.route('/post')
 def post():
     """Post Page"""
     return render_template('post.html')
+
+
+def send_email(name, email, phone, message):
+    # if you get UnicodeEncodeError
+    # try {message}".encode("utf-8")
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == '__main__':
